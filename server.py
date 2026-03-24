dfe9ac4c6ec6a91e1e069236c29fdd843ca9edaa
Shubham Kumar <kumarshubham73878@gmail.com>
2026-03-25 02:56:17 +0530

    fix: switch to native python server binding for railway

--- server.py
+++ server.py
@@ -131,4 +131,5 @@ def predict():
     return jsonify(res)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
